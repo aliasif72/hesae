1023be1370c1ca1142015882281507c56ecff376
dangerhesae <112620076+dangerhesae@users.noreply.github.com>
2023-05-22 05:14:11 +0530

    calling3fn.js

--- calling3fn.js
+++ calling3fn.js
@@ -1,25 +1,39 @@
-function buy(cb1,cb2)
-{
-    setTimeout(()=>
+    let posts=[
+        {user:'asif',time:'22 may'},
+        {user:'ali',time:'23 may' }];
+    
+    function createPosts(user)
     {
-        console.log("buy a car");
-        cb1(cb2);
-    },1000);
-}
-function plan(cb2)
-{
-    setTimeout(()=>
-    {
-        console.log("plan a trip");
-        cb2();
-    },2000);
-}
-function reach()
-{
-    setTimeout(()=>
-    {
-        console.log("reached manali");
-        
-    },3000);
+        return new Promise((resolve,reject)=>
+        {
+        setTimeout(()=>{
+            posts.push(user);
+            resolve();//resolve(user)
+        },1000)
+        })//.then(updateTime);
     }
-    buy(plan,reach);
+
+    function getPosts()
+    {
+            setTimeout(()=>{
+        let output='';
+        posts.forEach((us,index)=>
+            {
+                output+=`<li>${us.time}</li>`;
+            });
+            document.body.innerHTML=output;
+        },1000)
+        }
+
+    function updateTime()
+            {
+        return new Promise((resolve,reject)=>
+        {
+                setTimeout(()=>
+                {
+                    posts.time=new Date().getTime();
+                resolve(posts.time);
+            },1000)
+        })
+    }
+    Promise.all([createPosts({user:'asli',time:'24 may'}),updateTime()]).then(value=>console.log(value));
